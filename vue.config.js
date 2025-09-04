021b60d2e3f4fea9d119d5116dc710c0387c8f00
Khanh Duy Nguyen <kduyng71@gmail.com>
2025-09-04 17:57:29 -0500

    Fix JavaScript loading issues
    - Fix missing viewport meta tag content in public/index.html
    - Improve Vue config with proper build settings
    - Uncomment div#app wrapper in App.vue template
    - Disable source maps for production to reduce bundle size

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/SmartBookDemo/'
-    : '/'
+    : '/',
+  outputDir: 'dist',
+  assetsDir: '',
+  indexPath: 'index.html',
+  filenameHashing: true,
+  productionSourceMap: false
 }
